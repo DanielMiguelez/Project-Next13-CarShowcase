d60719907a7ddd52f94c96801a3bf90572fadd23
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-07-09 15:22:10 +0200

    setting queries

--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,8 @@ export interface CustomButtonProps {
     btnType? :"button" | "submit" | "reset" | undefined
 
 }
+
+export interface searchManufacturerProps {
+    manufacturer : string;
+    setManufacturer:(manufacturer:string) => void;
+}
